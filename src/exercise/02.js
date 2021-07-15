bc0276e5f891ce02e279d4d1e87733461c313cb8
diego <d.garciabrisa@gmail.com>
2021-07-15 13:03:19 +0200

    Module completed

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,18 +3,82 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+// function useLocalStorageState(key, defaultValue = '') {
+//   const [state, setState] = React.useState(
+//     () => window.localStorage.getItem(key) || defaultValue,
+//   )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+//   React.useEffect(() => {
+//     window.localStorage.setItem('name', state)
+//   }, [state, key])
+
+//   return [state, setState]
+// }
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = useLocalStorageState('name', initialName)
+
+//   console.log(name)
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+/**
+  4. 💯 flexible localStorage hook
+ */
+
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      try {
+        return deserialize(valueInLocalStorage)
+      } catch (error) {
+        window.localStorage.removeItem(key)
+      }
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [state, key, serialize])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  console.log(name)
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
